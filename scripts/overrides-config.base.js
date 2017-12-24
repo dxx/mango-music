c6e25c6170d41eb8a836278c98d690c120503d03
code-mcx <1827939333@qq.com>
2017-12-24 13:56:23 +0800

    Update scripts for production

--- scripts/overrides-config.base.js
+++ scripts/overrides-config.base.js
@@ -7,24 +7,9 @@ function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
-module.exports = function(config) {
-  // Define the root path alias
-  let alias = config.resolve.alias;
-  alias["@"] = resolve('src');
+module.exports.rootPath = resolve('src');
 
-  // Use your ESLint 
-  /*let eslintLoader = config.module.rules[0];
-  eslintLoader.use[0].options.useEslintrc = true;*/
-
-  // Add the stylus loader second-to-last
-  // (last one must remain as the "file-loader")
-  let loaderList = config.module.rules[1].oneOf;
-  loaderList.splice(loaderList.length - 1, 0, {
-    test: /\.styl$/,
-    use: ["style-loader", "css-loader", "stylus-loader"]
-  });
-  // Use Poststylus Plugin to handle stylus
-  config.plugins.splice(config.plugins.length, 0, new webpack.LoaderOptionsPlugin({
+module.exports.stylusLoaderOptionsPlugin = new webpack.LoaderOptionsPlugin({
     options: {
       stylus: {
         use: [
@@ -43,5 +28,4 @@ module.exports = function(config) {
         ]
       }
     }
-  }));
-}
+  });
--- scripts/overrides-config.dev.js
+++ scripts/overrides-config.dev.js
@@ -1,3 +1,21 @@
-const config = require('./overrides-config.base');
+const baseConfig = require('./overrides-config.base');
 
-module.exports = config;
+module.exports = function(config) {
+  // Define the root path alias
+  let alias = config.resolve.alias;
+  alias["@"] = baseConfig.rootPath;
+
+  // Use your ESLint 
+  /*let eslintLoader = config.module.rules[0];
+  eslintLoader.use[0].options.useEslintrc = true;*/
+
+  // Add the stylus loader second-to-last
+  // (last one must remain as the "file-loader")
+  let loaderList = config.module.rules[1].oneOf;
+  loaderList.splice(loaderList.length - 1, 0, {
+    test: /\.styl$/,
+    use: ["style-loader", "css-loader", "stylus-loader"]
+  });
+  // Use Poststylus Plugin to handle stylus
+  config.plugins.splice(config.plugins.length, 0, baseConfig.stylusLoaderOptionsPlugin);
+}
