946ea868432f287c1695130b53178f55eb4d7503
code-mcx <1827939333@qq.com>
2018-01-20 10:54:17 +0800

    Commit

--- src/components/singer/SingerList.js
+++ src/components/singer/SingerList.js
@@ -9,6 +9,8 @@ import {getSingerList} from "@/api/singer"
 import {CODE_SUCCESS} from "@/api/config"
 import * as SingerModel from "@/model/singer"
 
+import "core-js/es6/array"
+
 import "./singerlist.styl"
 
 class SingerList extends React.Component {
