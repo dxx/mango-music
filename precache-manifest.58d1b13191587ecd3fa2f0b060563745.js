a02292d5d3b705adb4f0adcd90c17b059ed02a68
code-mcx <1827939333@qq.com>
2020-01-09 11:34:26 +0800

    Init pages

---
+++ precache-manifest.58d1b13191587ecd3fa2f0b060563745.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bbbea131e64ade55b8a",
+    "url": "/mango-music/static/css/8.75f6e71b.chunk.css"
+  },
+  {
+    "revision": "49d7cb296ca29037a05e",
+    "url": "/mango-music/static/js/0.49d7cb29.chunk.js"
+  },
+  {
+    "revision": "8abf54e3e0e02dad0351",
+    "url": "/mango-music/static/js/1.8abf54e3.chunk.js"
+  },
+  {
+    "revision": "dc8bb5a68fdf3f496fff01da1376e426",
+    "url": "/mango-music/static/media/icomusic.dc8bb5a6.svg"
+  },
+  {
+    "revision": "7c2f8f7bbf7c91f3a6a3",
+    "url": "/mango-music/static/js/main.7c2f8f7b.chunk.js"
+  },
+  {
+    "revision": "43d977f81ccc4293c4b630d780280c91",
+    "url": "/mango-music/static/media/icomusic.43d977f8.ttf"
+  },
+  {
+    "revision": "e634cfec9f3a8906dc7d",
+    "url": "/mango-music/static/js/3.e634cfec.chunk.js"
+  },
+  {
+    "revision": "3207ca1a58ae1e36ad4bac122a6cfe9e",
+    "url": "/mango-music/static/media/icomusic.3207ca1a.woff"
+  },
+  {
+    "revision": "bc73496c9278398046b0",
+    "url": "/mango-music/static/js/4.bc73496c.chunk.js"
+  },
+  {
+    "revision": "344286e42d896e5f24add58e404a7f7a",
+    "url": "/mango-music/static/media/icomusic.344286e4.eot"
+  },
+  {
+    "revision": "7dc4f22956d12fb84976",
+    "url": "/mango-music/static/js/5.7dc4f229.chunk.js"
+  },
+  {
+    "revision": "2b774520b65e60e4b946",
+    "url": "/mango-music/static/js/runtime~main.2b774520.js"
+  },
+  {
+    "revision": "ce48fcf298801be0227a",
+    "url": "/mango-music/static/js/6.ce48fcf2.chunk.js"
+  },
+  {
+    "revision": "fa1c0623cf3f98ed13c5",
+    "url": "/mango-music/static/js/12.fa1c0623.chunk.js"
+  },
+  {
+    "revision": "25ea591af38dd5ebbc60",
+    "url": "/mango-music/static/js/7.25ea591a.chunk.js"
+  },
+  {
+    "revision": "14a307ddc7f29db17ac8",
+    "url": "/mango-music/static/js/11.14a307dd.chunk.js"
+  },
+  {
+    "revision": "7bbbea131e64ade55b8a",
+    "url": "/mango-music/static/js/8.7bbbea13.chunk.js"
+  },
+  {
+    "revision": "38fc329492a87223aaa6",
+    "url": "/mango-music/static/js/10.38fc3294.chunk.js"
+  },
+  {
+    "revision": "a631c10b1f1b9a69a5b7",
+    "url": "/mango-music/static/js/9.a631c10b.chunk.js"
+  },
+  {
+    "revision": "473725c8cff7cd5cde67592ffccd2a55",
+    "url": "/mango-music/static/img/play_bg.473725c8.jpg"
+  },
+  {
+    "revision": "a631c10b1f1b9a69a5b7",
+    "url": "/mango-music/static/css/9.c4d8a5d9.chunk.css"
+  },
+  {
+    "revision": "7c2f8f7bbf7c91f3a6a3",
+    "url": "/mango-music/static/css/main.7bd0cde9.chunk.css"
+  },
+  {
+    "revision": "25ea591af38dd5ebbc60",
+    "url": "/mango-music/static/css/7.293c37f8.chunk.css"
+  },
+  {
+    "revision": "ce48fcf298801be0227a",
+    "url": "/mango-music/static/css/6.77a26c0c.chunk.css"
+  },
+  {
+    "revision": "7dc4f22956d12fb84976",
+    "url": "/mango-music/static/css/5.eea5ecac.chunk.css"
+  },
+  {
+    "revision": "bc73496c9278398046b0",
+    "url": "/mango-music/static/css/4.14098a29.chunk.css"
+  },
+  {
+    "revision": "e634cfec9f3a8906dc7d",
+    "url": "/mango-music/static/css/3.0d446f55.chunk.css"
+  },
+  {
+    "revision": "fa1c0623cf3f98ed13c5",
+    "url": "/mango-music/static/css/12.bd04c5bd.chunk.css"
+  },
+  {
+    "revision": "8abf54e3e0e02dad0351",
+    "url": "/mango-music/static/css/1.a58a085e.chunk.css"
+  },
+  {
+    "revision": "0ad7ce68b9ab20871e1d1c740ef8c2e5",
+    "url": "/mango-music/index.html"
+  }
+];
